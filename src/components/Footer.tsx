b53f8a0666f4e2cb2a175da4baacf6bdc9023e1e
Solomon Karim <solomon.karim@atos.net>
2023-02-28 16:21:10 +0000

    Populate footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,41 @@
 export const Footer = () => {
-  return <div className="h-[16vh] bg-slate-700"></div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-slate-700 text-gray-200">
+      <div className="space-y-4 text-xs text-gray-200">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How MoviRank works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>MoviRank Plus</p>
+        <p>MoviRank Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-200">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>Movie Club</p>
+        <p>Referrals Accepted</p>
+        <p>This is not a real site</p>
+        <p>GitHub - solkarim91</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-200">
+        <h5 className="font-bold">REVIEW</h5>
+        <p>Become a trusted reviewer</p>
+        <p>Join Now</p>
+        <p>Lorem Ipsum</p>
+        <p>Dolor sit amet</p>
+        <p>Consectetur Adipisicing</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-200">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi</p>
+        <p>Easter Eggs</p>
+        <p>Delicious</p>
+      </div>
+    </div>
+  );
 };
